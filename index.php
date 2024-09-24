ac46aa9c9d5b8578f093d5a5cb20da55092d51a0
CodedEmmy <95436001+CodedEmmy@users.noreply.github.com>
2024-09-24 13:47:29 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,150 @@
+<?php
+ob_start();
+include_once("dbconfig.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Login - CrowdWise Prediction Tool</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <main>
+    <div class="container">
+
+      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+              <div class="d-flex justify-content-center py-4">
+                <a href="index.php" class="logo d-flex align-items-center w-auto">
+                  <img src="assets/img/logo.png" alt="">
+                  <span class="d-none d-lg-block">CrowdWise</span>
+                </a>
+              </div><!-- End Logo -->
+
+              <div class="card mb-3">
+
+                <div class="card-body">
+
+                  <div class="pt-4 pb-2">
+                    <h5 class="card-title text-center pb-0 fs-4">Decision-Making Tool</h5>
+                    <p class="text-center small">Login to your account</p>
+                  </div>
+				  
+				  <?php
+					$loginErr = false;
+					$errormsg = "";
+					if(isset($_POST['walletaddr'])){
+						$walletAddr = trim(mysqli_real_escape_string($conn,$_POST['walletaddr']));
+						$errormsg = trim(mysqli_real_escape_string($conn,$_POST['errmsg']));
+						if($walletAddr != "None"){
+							$userID = "";
+							$nickname = "";
+							$sql = "select user_id, nickname from user_accounts where wallet_address = '$walletAddr'";
+							$res = mysqli_query($conn, $sql);
+							if(@mysqli_num_rows($res) == 0){
+								$nickname = "CW_".substr($walletAddr,0, 7);
+								$insQuery = "insert into user_accounts(wallet_address, nickname, current_amt, withdrawn_amt) values('$walletAddr', '$nickname', '0', '0')";
+								mysqli_query($conn, $insQuery);
+								
+								$usql = "select user_id from user_accounts where wallet_address = '$walletAddr'";
+								$ures = mysqli_query($conn, $usql);
+								$pdata = mysqli_fetch_assoc($ures);
+								$userID = $pdata['user_id'];
+							}else{
+								$pdata = mysqli_fetch_assoc($res);
+								$userID = $pdata['user_id'];
+								$nickname = $pdata['nickname'];
+							}
+							session_start();
+							$_SESSION['u_id'] = $userID;
+							$_SESSION['u_nickname'] = $nickname;
+							$_SESSION['w_address'] = $walletAddr;
+							header("location: home.php");
+						}else{
+							$loginErr = true;
+						}
+					}                
+					if($loginErr){
+						echo "<div class='pt-4 pb-2' style='color: #cc0000;text-align:center;'>$errormsg</div><hr>";
+						echo "<div class='invalid-feedback'>$errormsg</div>";
+					}
+				?>
+
+                  <form class="row g-3 needs-validation" novalidate method="post" action="index.php" id="loginform">
+					<input type="hidden" name="walletaddr" id="walletaddr">
+					<input type="hidden" name="errmsg" id="errmsg">
+					
+                    <div class="col-12">
+                      <label for="yourUsername" class="form-label">Web3 Login</label>
+					  <div class="input-group has-validation">
+                        <img src="assets/img/login.jpg" alt="Login image">
+                      </div>
+                    </div>
+                    <div class="col-12">
+                      <button class="btn btn-primary w-100" onclick="walletLogin()" name="loginbtn">Login/Register</button>
+                    </div>
+                  </form>
+
+                </div>
+              </div>
+
+              <div class="credits">
+                Phantom Solana Wallet Required
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+      </section>
+
+    </div>
+  </main><!-- End #main -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+  
+  <script src="web3libs/web3utils.js"></script>
+  <script type="module">
+	function walletLogin()
+	{
+		const theForm = document.getElementById("loginform");
+		//theForm.addEventListener("submit",(e) => {e.preventDefault();});
+		let loginResp = phantomLogin();
+		theForm.errmsg.value = loginResp.errMessage;
+		theForm.walletaddr.value = loginResp.walletAddress;
+		theForm.submit();
+	}
+  </script>
+
+</body>
+
+</html>
