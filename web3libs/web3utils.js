990f76f9020485de4afc91c2df842d15a30784b1
CodedEmmy <95436001+CodedEmmy@users.noreply.github.com>
2024-09-26 09:21:42 +0100

    Add files via upload

--- web3libs/web3utils.js
+++ web3libs/web3utils.js
@@ -1,6 +1,8 @@
 //imports
 import nacl from './nacl-fast.js';
-import * from './solanaweb3.js';
+import * as web3 from './solanaweb3.js';
+import * from './Base58.min.js';
+
 
 async function phantomLogin()
 {
@@ -27,5 +29,67 @@ function signInWallet(walletAddr, theMsg)
 	const signedMessage = await window.solana.signMessage(walletAddr, encMessage);
 	//const signedMessage = await window.solana.signMessage(encMessage, "utf8");
 	const verified = nacl.sign.detached.verify(encMessage, signedMessage.signature, signedMessage.publicKey.toBytes());
+	if(walletAddr != signMessage.publicKey){
+		verified = false;
+	}
 	return verified;
+}
+
+function getEndPoint(epUrl)
+{
+	let url = web3.clusterApiUrl(epUrl);
+	return url;
+}
+
+function transferToUser(userAddr, amount, privkey, endpt)
+{
+	const connection = new web3.Connection(getEndPoint());
+	const privateKey = new Uint8Array(Base58.decode(privkey));
+	const dappAccount = web3.Keypair.fromSecretKey(privateKey);
+	const userWallet = new web3.Publickey(userAddr);
+	var signTrx = "-";
+	var fnErr = "-";
+	try{
+		(async() =>{
+			const transaction = new web3.Transaction();
+			transaction.add(web3.SystemProgram.transfer({fromPubkey: dappAccount.publicKey, toPubkey: UserWallet, lamports: amount}));
+			signature = await web3.sendAndConfirmTransaction(connection, transaction,[dappAccount],);
+			signTrx = signature.signature;
+			fnErr = "Confirmed";
+		})();
+	}catch(err){
+		signTrx = "-";
+		fnErr = err;
+	}
+	return {trxID: signTrx, errMessage: fnErr};
+}
+
+function transferFromUser(dappAddr, userAddr, amount, endpt)
+{
+	let txid = "-";
+	let fnMsg = "-";
+	const phantom = window.solana;
+	if(!phantom){
+		fnMsg = "Phantom Wallet not detected";
+	}else{
+		const userWallet = await phantom.publicKey;
+		if(userAddr != userWallet.publicKey){
+			fnMsg = "Wallet mismatch: Ensure that you are connected to " + userAddr;
+		}else{
+			var connection = new web3.Connection(getEndPoint(endpt),"confirmed");
+			var dappWallet = new web3.Publickey(dappAddr);
+			let transaction = new web3.Transaction().add(web3.SystemProgram.transfer({fromPubkey: userWallet, toPubkey: dappWallet, lamports: amount}));
+			transaction.feePayer = userWallet;
+			let blockhashObj = await connection.getRecentBlockhash();
+			transaction.recentBlockhash = blockhashObj.blockhash;
+			try{
+				let signature = await phantom.signAndSendTransaction(transaction);
+				await connection.confirmTransaction(signature.signature);
+				txid = signature.signature;
+			}catch(err){
+				fnMsg = "Error: " + err;
+			}
+		}
+	}
+	return {trx_id: txid, trx_status: fnMsg};
 }
