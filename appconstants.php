f945c13fff9cdf7d4ac00c0bcb5a02c7526a3899
CodedEmmy <95436001+CodedEmmy@users.noreply.github.com>
2024-10-10 03:01:30 +0100

    Update appconstants.php

--- appconstants.php
+++ appconstants.php
@@ -9,7 +9,8 @@ $DAPP_WALLET_ADDRESS = "Your dApp Solana Account Wallet Address";
 //For signing outgoing transactions
 $DAPP_PRIVATE_KEY = "Your dApp wallet private key";
 
-$RPC_ENDPOINT = "devnet"; //"mainnet-beta", or "Your preferred end point url string here"
+//$RPC_ENDPOINT = "devnet"; //"mainnet-beta", or "Your preferred end point url string here"
+$RPC_ENDPOINT = "https://rpc.ankr.com/solana_devnet";
 
 $MIN_DEPOSIT = 0.01 * $LAMPS_PER_SOL;
 $MIN_WITHDRAW = 0.007 * $LAMPS_PER_SOL;
